fcb0a4ee92b8d86fd234faa834a2c82aad4ffc20
Felipe Conceicao <felipecsilva207@gmail.com>
2020-08-10 21:02:19 -0300

    Colocando o CSS nas paginas

--- frontend02/src/Pages/Cadastrar/index.js
+++ frontend02/src/Pages/Cadastrar/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState} from "react";
 import ListaNegra from '../../services/ListaNegraApi';
 import {ToastContainer, toast} from 'react-toastify';
+import  './index.css';
 import 'react-toastify/dist/ReactToastify.css'
 
 const api = new ListaNegra()
-
 
 export default function Cadastrar() {
     const [nome, setNome] = useState("");
@@ -12,66 +12,65 @@ export default function Cadastrar() {
     const [inclusao, setInclusao] = useState();
     const [local, setLocal] = useState();
 
-
     const salvarClick = async () => {
         
-        const resp = await
-        api.cadastrar({
+      await api.cadastrar({
             nome: nome,
             motivo: motivo,
             inclusao: inclusao,
             local: local
-
         });
-
         toast.dark("Cadastrado na lista negra!!!")
     }
-
-
   
     return(
-        <div>
+        <div className="containerCadastrar">
+            <div className="containerCentroCadastrar">
+            
+                <div>
+                    <h2 className="tituloCadastrar">Cadastrar na Lista Negra</h2>
+                </div>
+            
+                <div className="inputsCadastrar">
+                    <label>Nome:&nbsp; &nbsp;</label>
+                    <input className="form-control" type="text"
+                    value={nome} 
+                    onChange={e => setNome(e.target.value)}
+                    />
+                </div>
+        
+                <div className="inputsCadastrar">
+                    <label>Motivo: &nbsp; </label>
+                    <input className="form-control" type="text"
+                    value={motivo}
+                    onChange={e => setMotivo(e.target.value)}
+                    />
+                </div>
+
+                <div className="inputsCadastrar">
+                    <label>Data:&nbsp;&nbsp;&nbsp; &nbsp;</label>
+                    <input className="form-control"type="date"
+                    value={inclusao}
+                    onChange={e => setInclusao(e.target.value)}
+                    />
+                </div>
+
+                <div className="inputsCadastrar">
+                    <label>Local: &nbsp; &nbsp;</label>
+                    <select className="form-control" onChange={e => setLocal(e.target.value)} id="local">
+                        <option value="Escola">Escola</option>
+                        <option value="Trabalho">Trabalho</option>
+                        <option value="Rua">Rua</option>
+                        <option value="Outro">Outro</option>
+                    </select>
+                </div>
+                
+                <div className="inputsCadastrar">
+                    <button className="btn btn-primary" onClick={salvarClick}>Cadastrar</button>
+                </div>
            
-            <div>
-                <h1>Cadastrar na Lista Negra</h1>
+                <ToastContainer/>
             </div>
-    
-            <div>
-                <label>Nome:</label>
-                <input type="text"
-                value={nome} 
-                onChange={e => setNome(e.target.value)}
-                />
-            </div>
-    
-            <div>
-                <label>Motivo:</label>
-                <input type="text"
-                value={motivo}
-                onChange={e => setMotivo(e.target.value)}
-                />
-            </div>
-
-            <div>
-                <label>Data:</label>
-                <input type="date"
-                value={inclusao}
-                onChange={e => setInclusao(e.target.value)}
-                />
-            </div>
-
-            <div>
-                <label>Local:</label>
-                <select onChange={e => setLocal(e.target.value)} id="local">
-                    <option value="Escola">Escola</option>
-                    <option value="Trabalho">Trabalho</option>
-                    <option value="Rua">Rua</option>
-                    <option value="Outro">Outro</option>
-                </select>
-            </div>
-    
-            <button onClick={salvarClick}>Cadastrar</button>
-            <ToastContainer/>
         </div>
     )
 }
--- frontend02/src/Pages/Consultar/index.js
+++ frontend02/src/Pages/Consultar/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useRef } from 'react';
 import ListaNegraApi from '../../services/ListaNegraApi'
 import LoadingBar from 'react-top-loading-bar';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import './index.css'
 
 const api = new ListaNegraApi();
 
@@ -28,54 +29,60 @@ export default function Consultar() {
 
     await api.deletar(id);
 
-    loadingBar.current.complete();
+    toast.dark("Excluido com sucesso!!!")
 
     consultarClick();
 
-    toast.dark("Excluido com sucesso");
+    loadingBar.current.complete();
   }
 
   
     
     return (
-      <div>
-        <LoadingBar
-          height={4}
-          color="#f11946"
-          ref={loadingBar}
-        />
-        <h1>Consultar na Lista Negra</h1>
+      <div className="containerConsultar">
+        <div className="containerCentroConsultar">
+              <LoadingBar
+                height={4}
+                color="#f11946"
+                ref={loadingBar}
+              />
+            
+              <h1 className="tituloConsultar">Consultar na Lista Negra</h1>
 
-        <div>
-          <button onClick={consultarClick}>Consultar</button>
-        </div>
-
-        <div>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Motivo</th>
-                <th>Inclusao</th>
-                <th>Local</th>
-                <th>Excluir</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {registros.map((item) => (
-                <tr key={item.id}>
-                  <td scope="row">#{item.id}</td>
-                  <td>{item.nome}</td>
-                  <td>{item.motivo}</td>
-                  <td>{new Date(item.inclusao + "Z").toDateString()}</td>
-                  <td>{item.local}</td>
-                  <td><button onClick={() => deletarClick(item.id)}>Deletar</button></td>
+             
+          <div className="tableConsultar">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Motivo</th>
+                  <th>Inclusao</th>
+                  <th>Local</th>
+                  <th>Excluir</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {registros.map((item) => (
+                  <tr key={item.id}>
+                    <td scope="row">#{item.id}</td>
+                    <td>{item.nome}</td>
+                    <td>{item.motivo}</td>
+                    <td>{new Date(item.inclusao + "Z").toDateString()}</td>
+                    <td>{item.local}</td>
+                    <td><button className="btn btn-danger" onClick={() => deletarClick(item.id)}>Deletar</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="btnConsultar">
+            <button class="btn btn-warning" onClick={consultarClick}>Consultar</button>
+          </div>
+          
+          <ToastContainer/>
         </div>
       </div>
     );
